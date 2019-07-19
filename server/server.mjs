bc83c540e92ed697ce57ed518b5eaf5cf94416dc
Steven Stromberg <gyozaguy@gmail.com>
2019-07-19 14:38:31 -0600

    Added port option

--- server/server.mjs
+++ server/server.mjs
@@ -5,4 +5,6 @@ import resolvers from './resolvers.mjs'
 const { GraphQLServer } = yoga
 const server = new GraphQLServer({ typeDefs, resolvers })
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start({ port: 4000 }, ({ port }) =>
+  console.log(`Server is running on localhost:${port}`)
+)
