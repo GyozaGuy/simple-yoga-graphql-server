99bff3960771c3e4451a8e7fc87a60472326a329
Steven Stromberg <gyozaguy@gmail.com>
2019-07-23 15:30:24 -0600

    Moved headers

--- public/helpers/fetchHelper.mjs
+++ public/helpers/fetchHelper.mjs
@@ -1,16 +1,16 @@
 export async function fetchGql(query) {
   return await fetchJson('/graphql', {
     body: JSON.stringify({ query }),
-    headers: {
-      'content-type': 'application/json'
-    },
     method: 'POST'
   })
 }
 
 export async function fetchJson(url, options) {
   const fetchFunc = options.fetch || fetch
-  const response = await fetchFunc(url, options)
+  const response = await fetchFunc(url, {
+    headers: { 'content-type': 'application/json' },
+    ...options
+  })
   return await response.json()
 }
 
